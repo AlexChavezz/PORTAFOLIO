16f00e120dddbeb8740d6b9d5857bbb97ac85075
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2021-05-22 21:49:06 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,17 +4,38 @@ function myFunction(x) {
     x.classList.toggle("change");
 }
 
+const menuList = document.querySelector('#header-list');
 
-const menuList =  document.querySelector('#header-list');
-const menu = document.querySelector('.container').addEventListener('click', (e) => {
-    
-   if(e.target.classList.contains('bar4')){
-    menuList.style.display = 'block';
-    e.target.className = 'bar-4';
-   }else{
+function clearVarNav(e) {
     menuList.style.display = 'none';
     e.target.className = 'bar4'
-   }
-   
+}
+function hiddenVarNav() {
+    const menu = document.querySelector('.bar-4');
+    menu.className = "bar4";
+    menuList.style.display = 'none';
+    const container = document.querySelector('.container');
+    container.classList.remove('change');
+}
+function showVarNav(e) {
+    menuList.style.display = 'block';
+    e.target.className = 'bar-4';
+}
+
+
+const menu = document.querySelector('.container').addEventListener('click', (e) => {
+
+    if (e.target.classList.contains('bar4')) {
+        showVarNav(e);
+    } else {
+        clearVarNav(e);
+    }
+
     
 });
+const listMenu = document.querySelectorAll('.list-group');
+for (let i = 0; i < listMenu.length; i++) {
+    listMenu[i].addEventListener('click', () => {
+        hiddenVarNav();
+    });
+}
