1ab3135b14643b7da58137b288608b2a05f292ed
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2021-05-22 19:59:53 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,20 +5,15 @@ function myFunction(x) {
 }
 
 
-const menu = document.createElement('div');
-menu.className = 'menuScreen';
+
 
 const container = document.querySelector('.container');
 container.addEventListener('click', (e) => {
     console.log(e.target)
     if(e.target.classList.contains("bar4")){
-        menu.style.display = 'block';
-        const body = document.querySelector('.menuComplete');
-        body.appendChild(menu);
-        e.target.className = 'bar-4';
+
     }else{
-        menu.style.display = 'none';
-        e.target.className = 'bar4';
+
     }
 
 
